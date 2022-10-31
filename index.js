40b7acfc4e72bcdfbb6d82c291a1d93fb49c3d29
Parshuram Gurav <44050904+parshuramgurav@users.noreply.github.com>
2022-10-31 13:47:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.http = (request, response) => {
-  response.status(200).send('Hello Serverless latest World!!!!!!!!!!!!!!!!');
+  response.status(200).send('Hello Serverless latest World!!!!!');
 };
 
 exports.event = (event, callback) => {
