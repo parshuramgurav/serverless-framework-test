739c58c26c92f163bc3d79b48e5c323455e1c764
Parshuram Gurav <parshuramgurav@FPRXDHF6GG.local>
2022-11-01 14:57:24 +0530

    update index.js & serverless.taml file

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict';
 
-exports.http = (request, response) => {
+exports.function-1 = (request, response) => {
   response.status(200).send('Hello Serverless Deployed by serverless framework');
 };
+
+// exports.function-2 = (request, response) => {
+//   response.status(200).send('Hello Serverless Deployed by serverless framework');
+// };
 
 exports.event = (event, callback) => {
   callback();
