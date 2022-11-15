f7c3bf4c22e7a7b05b10a4a2a90b8045b8de73d7
Parshuram Gurav <44050904+parshuramgurav@users.noreply.github.com>
2022-11-15 16:39:28 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.http = (request, response) => {
-  response.status(200).send('Hello Serverless World Deployed by serverless framework');
-};
-
-exports.event = (event, callback) => {
-  callback();
-};
